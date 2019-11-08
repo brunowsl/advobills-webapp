edc905836ce209e29686e646ce3717b18905ebd1
Bruno Lobão <brunowsl123@gmail.com>
2019-11-08 09:13:36 -0300

    adding a few configuration

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dense dark>
+      <v-toolbar-title>
+        <v-icon left>mdi-fingerprint</v-icon>
+        ADVObills</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
@@ -48,3 +25,9 @@ export default {
   data: () => ({})
 };
 </script>
+
+<style scoped>
+.txtpower {
+  color: beige;
+}
+</style>
