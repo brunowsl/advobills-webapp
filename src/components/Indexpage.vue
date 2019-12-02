013cd5e1809bcb8fde0adc3ef0c9937c6dac9242
Bruno Lobão <brunowsl123@gmail.com>
2019-12-02 09:19:36 -0300

    small sync

--- src/components/Indexpage.vue
+++ src/components/Indexpage.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <p> BEM-VINDO AO ADVOBILLS</p>
-    </div>
+    <v-container>
+        <div>
+            <p>This is an open source program made for personal use.
+                 Any intent of comercialization and monetization of this tool
+                 will be passive of legal.
+            </p>
+        </div> 
+        
+                   
+    </v-container>
 </template>
