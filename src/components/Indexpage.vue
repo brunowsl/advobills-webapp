fb09b2774575b29434c12db9996c9a5f71f5076c
Bruno Lobão <brunowsl123@gmail.com>
2019-12-02 09:24:23 -0300

    minimal changes

--- src/components/Indexpage.vue
+++ src/components/Indexpage.vue
@@ -1,18 +1,5 @@
 <template>
-<<<<<<< HEAD
-    <v-container>
-        <div>
-            <p>This is an open source program made for personal use.
-                 Any intent of comercialization and monetization of this tool
-                 will be passive of legal.
-            </p>
-        </div> 
-        
-                   
-    </v-container>
-=======
   <div>
     <p>BEM-VINDO AO ADVOBILLS</p>
   </div>
->>>>>>> 17bdf81d0851eb831a9e482e3619b3c44d5615ee
 </template>
